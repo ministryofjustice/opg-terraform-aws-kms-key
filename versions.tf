318f5f0d7b676a8a08ac29f77b624c981a30979b
radhia shah <radhia.shah@justice.gov.uk>
2026-02-06 16:38:28 +0000

    LPAL-1905 tf lint error review

--- versions.tf
+++ versions.tf
@@ -2,8 +2,8 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 6.28.0"
+      version = "6.28.0"
     }
   }
-  required_version = ">= 1.7.3"
+  required_version = "1.14.3"
 }
