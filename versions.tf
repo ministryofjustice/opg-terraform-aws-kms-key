cfe9816014f0f55e92510a16301db27ae026eeea
radhia shah <radhia.shah@justice.gov.uk>
2026-02-10 15:08:13 +0000

    github actions naming reverted to prev to match path

--- versions.tf
+++ versions.tf
@@ -1,9 +1,11 @@
 terraform {
+  required_version = "1.14.3"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "6.28.0"
     }
   }
-  required_version = "1.14.3"
+
 }
