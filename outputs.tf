6ef07e42f982976c7a5a9864ceb7182b691b7351
radhia shah <radhia.shah@justice.gov.uk>
2026-02-17 15:58:34 +0000

    replica region outputs changed

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,8 @@
-output "primary_key" {
-  value = aws_kms_key.primary
+output "primary_key_arn" {
+  value = aws_kms_key.primary.arn
 }
 
 output "replica_keys" {
   description = "List of replica KMS keys created in other regions"
-  value       = [for replica in aws_kms_replica_key.kms_key_replica : replica]
+  value       = values(aws_kms_replica_key.replica)[*]
 }
