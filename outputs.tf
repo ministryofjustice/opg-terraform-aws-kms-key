cec621b9ab317301560d56b7c1781ac5a798715c
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2026-05-07 21:40:31 +0100

    fix explicite example

--- outputs.tf
+++ outputs.tf
@@ -31,6 +31,6 @@ output "replica_key_for" {
 output "replica_key_map" {
   description = "All KMS replica keys created in other regions with the attributes of aws_kms_replica_key"
   value = {
-    eu-west-1 = aws_kms_replica_key.replica["eu-west-1"]
+    eu-west-2 = aws_kms_replica_key.replica["eu-west-2"]
   }
 }
