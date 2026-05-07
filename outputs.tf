6ccffdf64e01874e6380dbf878e156fa08f0c85f
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2026-05-07 21:38:10 +0100

    expriment with outputs

--- outputs.tf
+++ outputs.tf
@@ -3,12 +3,34 @@ output "primary_key" {
   value       = aws_kms_key.primary
 }
 
+output "key_alias" {
+  description = "The KMS key alias for the primary and all replica keys"
+  value       = "alias/${var.alias}"
+}
+
 output "replica_keys" {
+  description = "All KMS replica keys created in other regions with the attributes of aws_kms_replica_key"
+  value       = values(aws_kms_replica_key.replica)[*]
+}
+
+output "replica_keys_set" {
   description = "All KMS replica keys created in other regions with the attributes of aws_kms_replica_key"
   value       = toset(aws_kms_replica_key.replica[*])
 }
 
-output "replica_key_arns" {
+output "replica_keys_setoutside" {
   description = "All KMS replica keys created in other regions with the attributes of aws_kms_replica_key"
-  value       = { for k, v in aws_kms_replica_key.replica : k => v }
+  value       = toset(aws_kms_replica_key.replica)[*]
+}
+
+output "replica_key_for" {
+  description = "All KMS replica keys created in other regions with the attributes of aws_kms_replica_key"
+  value       = { for region, replica in aws_kms_replica_key.replica[*] : region => replica }
+}
+
+output "replica_key_map" {
+  description = "All KMS replica keys created in other regions with the attributes of aws_kms_replica_key"
+  value = {
+    eu-west-1 = aws_kms_replica_key.replica["eu-west-1"]
+  }
 }
