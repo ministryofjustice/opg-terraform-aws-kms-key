c2bbb6aa1db5df171f481e801c25d8109ee7295e
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2026-05-07 21:56:23 +0100

    cleanup

--- outputs.tf
+++ outputs.tf
@@ -12,15 +12,3 @@ output "replica_keys" {
   description = "All KMS replica keys created in other regions with the attributes of aws_kms_replica_key"
   value       = aws_kms_replica_key.replica
 }
-
-output "replica_keys_object" {
-  description = "All KMS replica keys created in other regions with the attributes of aws_kms_replica_key"
-  value       = aws_kms_replica_key.replica[*]
-}
-
-output "replica_key_map" {
-  description = "All KMS replica keys created in other regions with the attributes of aws_kms_replica_key"
-  value = {
-    eu-west-2 = aws_kms_replica_key.replica["eu-west-2"]
-  }
-}
