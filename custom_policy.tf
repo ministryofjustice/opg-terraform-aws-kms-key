d0efce4b987eff61dce80a35514c9541f2e495aa
radhia shah <radhia.shah@justice.gov.uk>
2026-05-18 16:04:55 +0100

    updating readme to add new custom policy document information

--- custom_policy.tf
+++ custom_policy.tf
@@ -1,5 +1,5 @@
 # # Merge the module's defined key policies with any local custom key policies provided by the user.
-# Set to null if no custom policies are provided.
+# Set to default if no custom policies are provided.
 data "aws_iam_policy_document" "kms_key_custom_policies" {
   source_policy_documents = concat(
     [data.aws_iam_policy_document.kms_key_module_policies.json],
